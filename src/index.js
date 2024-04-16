29b88cbed36444b082aeedb066cdb4488c73cc5c
Joshua <Joshuahale829@gmail.com>
2024-04-16 08:50:48 -0500

    updated index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Import React Library
+import React from 'react';
+
+// Import ReactDOM Library
+import ReactDOM from 'react-dom/client';
+
+// Import App component
+import App from './App';
+
+// Import CSS
+import './style/index.css';
+
+// Create root element using ReactDOM and select the HTML element with the id of 'root'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component to the root element
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
